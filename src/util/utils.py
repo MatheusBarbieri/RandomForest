b27d3d4bd0fb08d224aa2101631ac189e6416551
MatheusBarbieri <mcb_tk@hotmail.com>
2019-09-17 12:38:14 -0300

    fix: rename field

--- src/util/utils.py
+++ src/util/utils.py
@@ -1,5 +1,5 @@
 def group_by_attribute(attr, df):
-    if attr[1] == "categorical":
+    if attr[1] == "nominal":
         return df.groupby(attr[0])
     else:
         return df.groupby(df[attr[0]] < df[attr[0]].mean())
