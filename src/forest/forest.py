f9b613eeaca348e37dbf3332ba4a3697f06b4625
MatheusBarbieri <mcb_tk@hotmail.com>
2019-09-20 17:55:43 -0300

    feat: random forest predict

--- src/forest/forest.py
+++ src/forest/forest.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from sampling import generate_bootstraps
 from tree import Tree
 
@@ -10,3 +11,13 @@ class Forest:
     def generate(cls, train_set, attributes, ntree):
         bootstraps = generate_bootstraps(train_set, ntree)
         return Forest([Tree.generate(b, attributes) for b in bootstraps])
+
+    def predict(self, instance):
+        results = [tree.predict(instance) for tree in self.trees]
+        data = Counter(results)
+        result = max(results, key=data.get)
+        return result
+
+    def predict_df(self, instances):
+        instances['predicted'] = instances.apply(lambda x: self.predict(x), axis=1)
+        return instances[['class', 'predicted']]
