e33b1403641591c316b6639bd554c63d3d4cdb32
MatheusBarbieri <mcb_tk@hotmail.com>
2019-09-20 16:25:38 -0300

    feat: bootstraps uses random seed if none is given

--- src/sampling/bootstraps.py
+++ src/sampling/bootstraps.py
@@ -1,4 +1,7 @@
-def generate_bootstraps(df, n, seed=42):
+from numpy.random import randint
+
+
+def generate_bootstraps(df, n, seed=randint(10000)):
     bootstraps = []
     for i in range(n):
         sample = df.sample(frac=1, replace=True, random_state=seed+i)
