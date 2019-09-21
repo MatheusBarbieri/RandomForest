77f85fca02ee3445e408e24261d8f8004081d410
MatheusBarbieri <mcb_tk@hotmail.com>
2019-09-21 14:53:54 -0300

    feat: add printing to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from multiprocessing import Pool
+import pandas as pd
 import numpy as np
 import os
 import random
 import time
 
+from metrics import ConfusionMatrix
 from util import load_data, load_attributes, get_args
 from sampling import generate_k_folds, generate_splits
 from forest import Forest
@@ -11,29 +13,46 @@ from forest import Forest
 
 if __name__ == "__main__":
     args = get_args()
-    data = load_data(args.dataset)
+    data_path = args.dataset
+    data = load_data(data_path)
     attributes = load_attributes(args.attributes)
+
     seed = args.seed
+    k_folds_number = args.kfolds
+    trees_number = args.ntree
+    verbose = args.verbose or 0
     attributes_in_division = args.m if args.m else min(np.sqrt(len(attributes)), 3)
 
     if seed:
         print(f"Using seed: {seed}")
         random.seed(seed)
 
-    k_folds = generate_k_folds(data, args.kfolds, seed=seed)
+    k_folds = generate_k_folds(data, k_folds_number, seed=seed)
     splits = generate_splits(k_folds)
 
     with Pool(os.cpu_count()*2 - 1) as pool:
+        total_results = []
         for i, split in enumerate(splits):
             train, test = split
 
             start = time.time()
-            forest = Forest.generate(train, attributes, args.ntree, m=attributes_in_division, pool=pool)
+            forest = Forest.generate(train, attributes, trees_number, m=attributes_in_division, pool=pool)
             end = time.time()
-            print("Forest {} generation time: {}s".format(i+1, "{0:.4f}".format(end-start)))
+
+            if verbose > 1:
+                print("="*50)
+                print("Forest {} generation time: {}s".format(i+1, "{0:.3f}".format(end-start)))
 
             results = forest.predict_df(test)
+            total_results.append(results)
 
-            total = len(results)
-            correct = len(results[results['predicted'] == results['class']])
-            print(f"Total: {total}, classified correctly: {correct}")
+            if verbose > 1:
+                confusion_matrix = ConfusionMatrix(results)
+                confusion_matrix.show(verbose=(verbose > 2))
+
+        final_confusion_matrix = ConfusionMatrix(pd.concat(total_results))
+        if verbose > 1:
+            print("="*50)
+        print(f"Results for {data_path.replace('.csv', '')}:")
+        print(f"Params: k_folds: {k_folds_number}; ntree: {trees_number}; m: {attributes_in_division}; seed: {seed}")
+        final_confusion_matrix.show(verbose=(verbose > 0))
