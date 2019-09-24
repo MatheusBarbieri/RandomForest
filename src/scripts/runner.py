34a07ed9158cb54203090f2fe60e75c94d2df3c1
MatheusBarbieri <mcb_tk@hotmail.com>
2019-09-24 14:01:51 -0300

    chore: update testing script parameters

--- src/scripts/runner.py
+++ src/scripts/runner.py
@@ -3,44 +3,37 @@ import subprocess
 BASE_PATH = './data'
 
 instances = [
-    'dataset_31_credit-g',
     'dataset_44_spambase',
+    'dataset_31_credit-g',
     'dataset_191_wine',
     'phpOkU53r'
     ]
 
-instances_m = [
-    [0, 3, 4, 5, 7, 8],
-    [0, 3, 5, 10, 15],
-    [0, 2, 3, 5, 6, 7],
-    [0, 2, 3, 4, 5]
-]
+number_of_executions_with_each_params = 50
+exec_count = 1
 
-number_of_executions_with_each_params = 5
-
-k_folds = [3, 5, 7, 10]
+k_folds = 5
 ntree = [5, 10, 15, 25, 50, 100, 250, 500, 1000, 2000, 3000, 5000, 10000]
+
+total = len(instances) * number_of_executions_with_each_params * len(ntree)
 
 for current_n_tree in ntree:
     for e in range(number_of_executions_with_each_params):
-        for current_k_folds in k_folds:
-            for instance, im in zip(instances, instances_m):
-                current_dataset = f"{BASE_PATH}/{instance}.csv"
-                current_attributes = f"{BASE_PATH}/{instance}.json"
-                for current_m in im:
+        for instance in instances:
+            current_dataset = f"{BASE_PATH}/{instance}.csv"
+            current_attributes = f"{BASE_PATH}/{instance}.json"
 
-                    execution = [
-                        'python', 'src/main.py',
-                        '-d', str(current_dataset),
-                        '-a', str(current_attributes),
-                        '-k', str(current_k_folds),
-                        '-n', str(current_n_tree),
-                        '-p'
-                        ]
+            print(f'Execução [{exec_count}/{total}]')
+            exec_count = exec_count + 1
 
-                    if current_m != 0:
-                        execution.append('-m')
-                        execution.append(str(str(current_m)))
+            execution = [
+                'python3', 'src/main.py',
+                '-d', str(current_dataset),
+                '-a', str(current_attributes),
+                '-k', str(k_folds),
+                '-n', str(current_n_tree),
+                '-p'
+                ]
 
-                    process = subprocess.Popen(execution)
-                    process.wait()
+            process = subprocess.Popen(execution)
+            process.wait()
