daf2f10b098c303e4245ebab540975ce13ff4f14
Carolina Mello <carolina.mello@corp.globo.com>
2019-09-30 21:08:55 -0300

    fix: gain fixed

--- src/information/gain.py
+++ src/information/gain.py
@@ -54,7 +54,8 @@ def info_attributes(data, attributes_index, attributes_kinds, attributes_list, c
 
 def attributes_gains(data, attributes_index, attributes_kinds, attributes_list, class_index):
     total_size = data.shape[0]
-    classes, counts = np.unique(class_index, return_counts=True)
+    classes, counts = np.unique(data[:, class_index], return_counts=True)
     df_info = np_info(counts, total_size)
-    attr_infos = info_attributes(data, attributes_index, attributes_kinds, attributes_list, class_index)
-    return [df_info - info for info in attr_infos]
+    attr_infos, all_groups, all_groups_index = info_attributes(data, attributes_index, attributes_kinds, attributes_list, class_index)
+    gains = [df_info - info for info in attr_infos]
+    return gains, all_groups, all_groups_index
